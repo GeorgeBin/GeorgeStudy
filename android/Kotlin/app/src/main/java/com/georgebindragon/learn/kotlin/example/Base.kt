eca039946aa44d3c8a0fe407963e0708af772ec4
GeorgeBinDragon <georgebindragon@gmail.com>
2022-05-25 18:00:15 +0800

    init project

---
+++ android/Kotlin/app/src/main/java/com/georgebindragon/learn/kotlin/example/Base.kt
@@ -0,0 +1,21 @@
+package com.georgebindragon.learn.kotlin.example
+
+/**
+ * author：
+ *
+ * description：
+ * action：
+ *
+ * modification：
+ */
+
+
+class Base {
+
+    // variable
+    var int: Int = 1;
+    var int2 = 2 // ignore semicolon and 
+    val int3 = 3
+
+    //
+}
