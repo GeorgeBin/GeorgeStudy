41fb5bd997fe4db4e70ca5e6ca77244907a1a79c
GeorgeBinDragon <georgebindragon@gmail.com>
2022-06-30 10:41:46 +0800

    :tada: 初始化

---
+++ android/LearnJNI/app/src/main/java/com/example/learnjni/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.learnjni;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.learnjni.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity
+{
+
+	// Used to load the 'learnjni' library on application startup.
+	static
+	{
+		System.loadLibrary("learnjni");
+	}
+
+	private ActivityMainBinding binding;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+
+		binding = ActivityMainBinding.inflate(getLayoutInflater());
+		setContentView(binding.getRoot());
+
+		// Example of a call to a native method
+		TextView tv = binding.sampleText;
+		tv.setText(stringFromJNI());
+	}
+
+	/**
+	 * A native method that is implemented by the 'learnjni' native library,
+	 * which is packaged with this application.
+	 */
+	public native String stringFromJNI();
+}
