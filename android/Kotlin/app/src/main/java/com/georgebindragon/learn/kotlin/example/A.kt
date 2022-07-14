9765e4287ab94a2980e4c08ca3742ff5f2b8a93a
GeorgeBinDragon <georgebindragon@gmail.com>
2022-07-14 11:14:53 +0800

    :tada: 学习Kotlin

---
+++ android/Kotlin/app/src/main/java/com/georgebindragon/learn/kotlin/example/A.kt
@@ -0,0 +1,27 @@
+package com.georgebindragon.learn.kotlin.example
+
+/**
+ * author：
+ *
+ * description：
+ * action：
+ *
+ * modification：
+ */
+
+
+class A {
+
+    // 变量
+    var int: Int = 1;
+    var int2 = 2 // 可以自动推测变量类型
+    val int3 = 3 // 只读
+
+    @Override
+    fun test() {
+
+        var color = 1
+        color = 2
+
+    }
+}
---
+++ android/Kotlin/app/src/main/java/com/georgebindragon/learn/kotlin/example/ColorScheme.kt
@@ -0,0 +1,29 @@
+package com.georgebindragon.learn.kotlin.example
+
+/**
+ *
+ * 创建人：George
+ *
+ * 描述：
+ *
+ * 修改人：
+ * 修改时间：
+ * 修改备注：
+ */
+
+
+class ColorScheme {
+    var test1 = 1
+    val test2 = "String name"
+
+    fun funName() {
+        var test3 = 1
+        val test4 = "String name2"
+
+        test1 = 1 - 2
+        test3 = 1 - 2
+
+        println(test1)
+        println(test3)
+    }
+}
