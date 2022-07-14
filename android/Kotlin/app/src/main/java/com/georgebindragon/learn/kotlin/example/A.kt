01c056b3d690aedb9e0a222e860dab6a617852a5
GeorgeBinDragon <georgebindragon@gmail.com>
2022-07-14 11:38:33 +0800

    :memo: private 修饰，则不自动生成 getter、setter

--- android/Kotlin/app/src/main/java/com/georgebindragon/learn/kotlin/example/A.kt
+++ android/Kotlin/app/src/main/java/com/georgebindragon/learn/kotlin/example/A.kt
@@ -16,6 +16,7 @@ class A {
     var int: Int = 1;
     var int2 = 2 // 可以自动推测变量类型
     val int3 = 3 // 只读
+    private val test4 = "hello" // private 修饰，则不自动生成 getter、setter
 
     @Override
     fun test() {
