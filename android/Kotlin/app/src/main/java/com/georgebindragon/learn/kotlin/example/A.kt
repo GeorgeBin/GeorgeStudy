c520bd24cb8bbd95e59309e7550c31481ff773b5
GeorgeBinDragon <georgebindragon@gmail.com>
2022-07-14 14:43:26 +0800

    :art: 按照Kotlin的代码规范格式化

--- android/Kotlin/app/src/main/java/com/georgebindragon/learn/kotlin/example/A.kt
+++ android/Kotlin/app/src/main/java/com/georgebindragon/learn/kotlin/example/A.kt
@@ -9,20 +9,19 @@ package com.georgebindragon.learn.kotlin.example
  * modification：
  */
 
-
 class A {
 
-    // 变量
-    var int: Int = 1;
-    var int2 = 2 // 可以自动推测变量类型
-    val int3 = 3 // 只读
-    private val test4 = "hello" // private 修饰，则不自动生成 getter、setter
+  // 变量
+  var int: Int = 1;
+  var int2 = 2 // 可以自动推测变量类型
+  val int3 = 3 // 只读
+  private val test4 = "hello" // private 修饰，则不自动生成 getter、setter
 
-    @Override
-    fun test() {
+  @Override
+  fun test() {
 
-        var color = 1
-        color = 2
+    var color = 1
+    color = 2
 
-    }
+  }
 }
